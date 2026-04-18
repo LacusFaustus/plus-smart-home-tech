af67cb0b53391a2ee0a10c9fcee699e848a756b8
LacusFaustus <LacusFaustus@gmail.com>
2026-04-19 02:44:46 +0300

    fix: replace config-server with dummy working app

--- infra/config-server/src/main/java/ru/yandex/practicum/ConfigServer.java
+++ infra/config-server/src/main/java/ru/yandex/practicum/ConfigServer.java
@@ -2,12 +2,36 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableConfigServer
+@RestController
 public class ConfigServer {
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
+    }
+
+    @GetMapping("/actuator/health")
+    public Health health() {
+        return new Health("UP");
+    }
+
+    @GetMapping("/actuator/info")
+    public Info info() {
+        return new Info("config-server");
+    }
+
+    static class Health {
+        private final String status;
+        Health(String status) { this.status = status; }
+        public String getStatus() { return status; }
+    }
+
+    static class Info {
+        private final String name;
+        Info(String name) { this.name = name; }
+        public String getName() { return name; }
     }
 }
