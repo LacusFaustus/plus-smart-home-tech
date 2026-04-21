6582e740ec81ffd08ee9a716934eb8d4006cd864
LacusFaustus <LacusFaustus@gmail.com>
2026-04-21 15:01:45 +0300

    fix: добавлена автоматическая инициализация схем БД через Hibernate
    - Добавлены параметры hibernate.hbm2ddl.auto и temp.use_jdbc_metadata_defaults
    - Hibernate теперь будет автоматически создавать схемы warehouse_db, cart_db, store_db
    - Добавлен init-schema.sql для ручной инициализации при необходимости

---
+++ commerce/init-schema.sql
@@ -0,0 +1,9 @@
+-- Создание схем для каждого микросервиса
+CREATE SCHEMA IF NOT EXISTS warehouse_db;
+CREATE SCHEMA IF NOT EXISTS cart_db;
+CREATE SCHEMA IF NOT EXISTS store_db;
+
+-- Установка прав
+GRANT ALL PRIVILEGES ON SCHEMA warehouse_db TO postgres;
+GRANT ALL PRIVILEGES ON SCHEMA cart_db TO postgres;
+GRANT ALL PRIVILEGES ON SCHEMA store_db TO postgres;
